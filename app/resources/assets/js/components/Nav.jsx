6f376bc5535ea80160c2db9db06dd3b1a629eca6
Leo Knudsen <nightskater-220@hotmail.com>
2017-01-10 14:21:19 +0100

    put styling into navbar

--- app/resources/assets/js/components/Nav.jsx
+++ app/resources/assets/js/components/Nav.jsx
@@ -3,18 +3,36 @@ var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
 const Nav = (props) => {
-	var styles = {
-		float: 'left'
-	};
 
 	return(
-		<div>
-			<IndexLink to="/" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>Get weather</IndexLink>
-			<Link to="/about" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>About</Link>
-			<Link to="/examples" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>Examples</Link>
-			<h2 style={styles}>ReactWeather</h2>
-		</div>
+	       <div className="top-bar">
+	       		<div className="top-bar-left">
+	       			<ul className="menu">
+	       				<li className="menu-text">React Weather</li>
+	       				<li>
+	       					<IndexLink to="/" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>Get weather</IndexLink>
+	       				</li>
+	       				<li>
+	       					<Link to="/about" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>About</Link>
+	       				</li>
+	       				<li>
+	       					<Link to="/examples" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>Examples</Link>
+	       				</li>
+	       			</ul>
+	       		</div>
+	       		<div className="top-bar-right">
+
+	       		</div>
+	       </div>
 	);
 };
+
+const oldNav = (
+<div>
+	<IndexLink to="/" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>Get weather</IndexLink>
+	<Link to="/about" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>About</Link>
+	<Link to="/examples" activeClassName="active" activeStyle={ {fontWeight: 'bold'} }>Examples</Link>
+</div>
+);
 
 module.exports = Nav;
